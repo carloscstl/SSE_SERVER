1ff9a9d06018048096c7ed4b36fb7866ae82b60d
Carlos <carcastillo0110@gmail.com>
2022-10-04 21:18:49 -0500

    Isussues

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,29 +1,22 @@
-const { Router } = require('express');
-const { check } = require('express-validator');
-const { register, login, getListRoles, renewToken } = require('../controllers/auth.controller');
-const { validateJWT } = require('../middleware/jwt.middleware');
-const { validateFields } = require('../middleware/validate-fields.middleware');
+const { Router } = require("express");
+const { check } = require("express-validator");
+const { login, renewToken } = require("../controllers/auth.controller");
+const { validateJWT } = require("../middleware/jwt.middleware");
+const { validateFields } = require("../middleware/validate-fields.middleware");
 //const { validarJWT, validarADMIN_ROLE } = require('../middlewares/validar-jwt.middleware');
-
 
 const router = Router();
 
-
-router.post('/login', [
-    check('username', 'El username es requerido.').not().isEmpty(),
-    check('password', 'El password es requerido.').not().isEmpty(),
+router.post(
+  "/login",
+  [
+    check("username", "El username es requerido.").not().isEmpty(),
+    check("password", "El password es requerido.").not().isEmpty(),
     validateFields,
-], login);
+  ],
+  login
+);
 
-
-
-
-
-
-router.get('/renew', validateJWT, renewToken);
-
-//router.get('/roles', getListRoles);
-
-
+router.get("/renew", validateJWT, renewToken);
 
 module.exports = router;
